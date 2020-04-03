34df05e3e730f34cb8704b4e0990238e75df66df
Toon Van den Bos <toon@whitecube.be>
2020-04-03 10:20:37 +0200

    Added WP & theme installation

---
+++ wp-content/themes/courswp/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World</title>
+</head>
+<body>
+    <h1><?php bloginfo('name'); ?></h1>
+    <p><?php bloginfo('description'); ?></p>
+</body>
+</html>
