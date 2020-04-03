0104f89e0e5ea7999b57894f71139157c1746c1d
Toon Van den Bos <toon@whitecube.be>
2020-04-03 11:40:59 +0200

    Added posts preview & permalink on index page

--- wp-content/themes/courswp/index.php
+++ wp-content/themes/courswp/index.php
@@ -1,8 +1,15 @@
 <?php get_header(); ?>
 
     <?php if(have_posts()): while(have_posts()): the_post(); ?>
-        <h1><?php the_title(); ?></h1>
-        <div><?php the_content(); ?></div>
+
+        <article class="post">
+            <h2 class="post__title"><?php the_title(); ?></h2>
+            <div class="post__excerpt"><?php the_excerpt(); ?></div>
+            <footer class="post__cta">
+                <a href="<?php the_permalink(); ?>" class="post__link">Voir l'article <span class="sro">"<?php the_title(); ?>"</span> en entier</a>
+            </footer>
+        </article>
+
     <?php endwhile;endif; ?>
 
 <?php get_footer(); ?>
