f86cad1af47f04b171291d0812e7578c3e867e2a
Toon Van den Bos <toon@whitecube.be>
2020-04-24 09:27:42 +0200

    Added page title generator helper function

--- wp-content/themes/courswp/functions.php
+++ wp-content/themes/courswp/functions.php
@@ -3,3 +3,19 @@
 function cw_get_theme_asset($asset) {
     return get_stylesheet_directory_uri() . '/' . ltrim($asset, '/');
 }
+
+function cw_get_title($separator = '•', $displayTitleLeft = true) {
+    $separator = ' ' . $separator . ' ';
+
+    $title = trim(wp_title('', false, 'right'));
+
+    if(!$title) {
+        return get_bloginfo('name');
+    }
+
+    if($displayTitleLeft) {
+        return $title . $separator . get_bloginfo('name');
+    } else {
+        return get_bloginfo('name') . $separator . $title;
+    }
+}
--- wp-content/themes/courswp/header.php
+++ wp-content/themes/courswp/header.php
@@ -2,13 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>TO DO</title>
+    <title><?= cw_get_title(); ?></title>
     <?php wp_head(); ?>
     <link rel="stylesheet" href="<?= cw_get_theme_asset('assets/css/style.css'); ?>" />
     <script src="<?= cw_get_theme_asset('assets/js/script.js'); ?>"></script>
 </head>
 <body>
     <header class="header">
-        <h1><?php bloginfo('name'); ?></h1>
+        <h1><?= cw_get_title('-', false); ?></h1>
     </header>
     <main class="page">
